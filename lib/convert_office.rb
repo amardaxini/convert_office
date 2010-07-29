58c786c9f58dfc59b7b9a2f7571b946f088f4e5e
amardaxini <amardaxini@gmail.com>
2010-07-29 17:02:47 +0530

    first commi t

---
+++ lib/convert_office.rb
@@ -0,0 +1,67 @@
+require 'convert_office_config'
+module ConvertOffice
+	class ConvertOfficeFormat
+		JAR_PATH= File.expand_path(File.join(File.dirname(__FILE__),"java","jar","convert_office.jar"))
+		TEXT_FORMAT = %w(pdf odt sxw rtf  doc txt html wiki)
+		XL_FORMAT = %w(pdf ods sxc xls csv tsv html)
+		PPT_FORMAT = %w(pdf swf odp sxi ppt html)
+		DRAWING_FORMAT = %w(svg swf)
+		VALID_FORMAT=	{
+						"odt"=>TEXT_FORMAT,
+						"sxw"=>TEXT_FORMAT,
+						"rtf"=>TEXT_FORMAT,
+						"doc"=>TEXT_FORMAT,
+						"wpd"=>TEXT_FORMAT,
+						"txt"=>TEXT_FORMAT,
+						"html"=>TEXT_FORMAT,
+						"htm"=>TEXT_FORMAT,
+						"ods"=>XL_FORMAT,
+						"sxc"=>XL_FORMAT,
+						"xls"=>XL_FORMAT,
+						"csv"=>XL_FORMAT,
+						"tsv"=>XL_FORMAT,
+						"odp"=>PPT_FORMAT,
+						"sxi"=>PPT_FORMAT,
+						"ppt"=>PPT_FORMAT,
+						"odg"=>DRAWING_FORMAT
+		}
+		def convert(input_file,output_file="",format="")
+			java_bin = ConvertOffice::ConvertOfficeConfig.options[:java_bin]
+			port_no = ConvertOffice::ConvertOfficeConfig.options[:soffice_port]
+			nailgun = ConvertOffice::ConvertOfficeConfig.options[:nailgun]
+			input_extension_name = File.extname(input_file).split(".").last
+			if format.blank? && output_file.blank?
+				raise ArgumentError=>"Please provide format or output file"
+			elsif output_file.blank?
+				check_valid_conversion(input_extension_name,format)
+				if nailgun
+          command = "#{Nailgun::NgCommand::NGPATH} con -p #{port_no} -f #{format} #{input_file}"
+				else
+					command = "#{java_bin} -Xmx512m -Djava.awt.headless=true -cp #{JAR_PATH} com.artofsolving.jodconverter.cli.ConvertDocument -p #{port_no} -f #{format} #{input_file}"
+				end	
+			elsif format.blank?
+				output_format = File.extname(output_file).split(".").last
+				check_valid_conversion(input_extension_name,output_format)
+				if nailgun
+					command = "time #{Nailgun::NgCommand::NGPATH} con -p #{port_no} -p #{port_no} #{input_file} #{output_file}"
+				else
+					command = "time #{java_bin} -Xmx512m -Djava.awt.headless=true -cp #{JAR_PATH} com.artofsolving.jodconverter.cli.ConvertDocument -p #{port_no} #{input_file} #{output_file}"
+				end
+			end
+			puts command
+			system(command)
+		end
+
+		def check_valid_conversion(input_ext,format)
+			if VALID_FORMAT[input_ext].nil?
+				puts "Please provide proper input file"
+				puts "Input file type #{VALID_FORMAT.keys.join(",")}"
+				exit(1)
+			elsif	!VALID_FORMAT[input_ext].include?(format)
+				puts "Please provide proper output format/output file"
+				puts "Format/Output file must be #{VALID_FORMAT[input_ext].join(",")}"
+				exit(1)
+			end
+		end
+	end
+end
